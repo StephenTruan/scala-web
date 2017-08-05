31a70b1db0caee203ef8c3d0ab0923f1933ba5fe
wujianchuan <wujianchuan@shinow.com>
2017-08-05 14:54:35 +0800

    将基于内存的权限管理修改为基于数据库的权限管理。

--- src/main/scala/com/cc/enties/User.scala
+++ src/main/scala/com/cc/enties/User.scala
@@ -1,38 +1,39 @@
 package com.cc.enties
 
 import java.util.Date
+
 import scala.beans.BeanProperty
-import javax.persistence.Id
-import javax.persistence.GeneratedValue
+import javax.persistence._
+
 import org.hibernate.validator.constraints.NotEmpty
-import javax.persistence.Entity
-import javax.persistence.Table
 import org.springframework.format.annotation.DateTimeFormat
 import javax.validation.constraints.NotNull
+
 import org.hibernate.validator.constraints.NotBlank
 
-@Table(name = "users")
+@Table(name = "TBL_USER")
 @Entity
 class User {
 
   @Id
   @GeneratedValue
   @BeanProperty
+  @Column(name = "UUID")
   var id: Long = _
-  
+
   @BeanProperty
   @NotBlank
   var name: String = _
-  
+
   @BeanProperty
   @NotNull
-  @DateTimeFormat(pattern="yyyy-MM-dd")
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
   var birthday: Date = _
-  
+
   @BeanProperty
   @NotEmpty
   var telephone: String = _
-  
+
   override def toString = "id = " + id + ",name = " + name + ",birthday = " + birthday + ",telephone = " + telephone
-  
+
 }
--- src/main/scala/com/cc/scalaWebSecurity.scala
+++ src/main/scala/com/cc/scalaWebSecurity.scala
@@ -1,5 +1,7 @@
 package com.cc
 
+import org.apache.tomcat.jdbc.pool.DataSource
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -10,22 +12,35 @@ import org.springframework.security.config.annotation.web.configuration.{EnableW
 @EnableWebSecurity
 class scalaWebSecurity extends WebSecurityConfigurerAdapter {
 
+  @Autowired val dataSource: DataSource = null
+
   override protected def configure(auth: AuthenticationManagerBuilder): Unit = {
     auth
-      .inMemoryAuthentication()
-      .withUser("user").password("123456").roles("USER").and()
-      .withUser("admin").password("admin").roles("ADMIN", "USER")
+      .jdbcAuthentication().dataSource(dataSource)
+      .usersByUsernameQuery(
+        "SELECT NAME, PASSWORD, STATE FROM TBL_USER WHERE NAME =?"
+      )
+      .authoritiesByUsernameQuery(
+        "SELECT u. NAME, r.ROLE_NAME FROM TBL_USER u RIGHT JOIN TBL_ROLE r ON u.ROLE_ID = r.UUID WHERE NAME = ?"
+      )
   }
 
   override protected def configure(http: HttpSecurity): Unit = {
     http
-      .csrf().disable()
-      .formLogin().and()
-      .authorizeRequests()
-      .antMatchers(HttpMethod.GET, "/user/**", "/score/**").authenticated()
-      .antMatchers(HttpMethod.POST).hasRole("ADMIN")
-      .anyRequest().permitAll()
+      .csrf().disable() //禁用CSRF token
+      .formLogin() //添加默认登录界面
       .and()
+      .httpBasic() //启用HTTP Basic认证
+      .and()
+      .rememberMe()
+      .tokenValiditySeconds(2419200).key("myKey") //将token存储在cookie中(登录请求必须包含一个名为remember_me的参数)
+      .and()
+      .authorizeRequests()
+      .antMatchers(HttpMethod.GET, "/user/list").authenticated()
+      .antMatchers(HttpMethod.GET, "/score/list").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+      .antMatchers(HttpMethod.GET, "/user/find/**").hasAuthority("ROLE_ADMIN")
+      .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+      .anyRequest().permitAll();
   }
 
 }
